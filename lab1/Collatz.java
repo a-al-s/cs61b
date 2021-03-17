2344797f9e0c13a826398bde92581a09b1df848b
a-al-s <abdullah_al-s@outlook.com>
2021-03-17 12:01:03 +0300

    lab1 solved

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,18 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** This method prints Collatz sequence. The sequnce determine the next value based on the previous one
+     *  if the previous value:
+     *      even: divide it by 2
+     *      odd: multiply it by 3 then add 1
+     *  The sequence considered over when it reached 1 */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return (n * 3) + 1;
         }
+        
     }
 
     public static void main(String[] args) {
