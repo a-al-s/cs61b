190ca82c0b259976efc667f601d7b23cb3b85139
a-al-s <abdullah_al-s@outlook.com>
2021-04-09 10:44:08 +0300

    adding large numbers test

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,10 +1,6 @@
 package deque;
 
 public class LinkedListDeque <T> {
-    /*
-    - add a private class like IntNode
-    - learn Generic type
-     */
     private class Node {
         private T item;
         private Node next;
@@ -21,18 +17,75 @@ public class LinkedListDeque <T> {
     private int size;
 
     public LinkedListDeque() {
+        // the next and prev can be null but I think for circular list it should be the same sentinel
+        // this.sentinel
         this.sentinel = new Node(null, null, null);
         this.size = 0;
     }
 
-    public LinkedListDeque(T item) {
-        this.sentinel = new Node(null, null, null);
-        sentinel.next = new Node(item, null, sentinel);
-        this.size = 1;
-    }
-
     public boolean isEmpty() {
         return (this.size == 0);
+    }
+
+    public void addFirst(T item) {
+        if (size == 0) {
+            sentinel.next = new Node(item, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+            this.size++;
+        } else {
+            Node n = sentinel.next;
+            sentinel.next = new Node(item, n, sentinel);
+            this.size++;
+        }
+    }
+
+    public void addLast(T item) {
+        if (size == 0) {
+            sentinel.next = new Node(item, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+            this.size++;
+        } else {
+            Node p = sentinel.prev;
+            sentinel.prev = new Node(item, sentinel, p);
+            this.size++;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if (this.size > 0) {
+            Node first = sentinel.next;
+            while (first.item != null) {
+                System.out.print(first.item + " ");
+                first = first.next;
+            }
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        } else {
+            Node firstItem = sentinel.next;
+            sentinel.next = firstItem.next;
+            this.size--;
+            return firstItem.item;
+        }
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        } else {
+            Node lastItem = sentinel.prev;
+            sentinel.prev = lastItem.prev;
+            this.size--;
+            return lastItem.item;
+        }
     }
 
 }
